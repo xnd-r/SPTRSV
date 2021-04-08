de0631e7d25692f1247fd8026429868577abf00a
Alexander Romanov <romanov_a@master.nngu>
2021-04-09 00:22:10 +0300

    KMP_AFFINITY=granularity=fine,compact,1,0

--- run.py
+++ run.py
@@ -27,37 +27,38 @@ def get_times(filename, algos, matrices, repeats, threads):
 	run_name = datetime.datetime.now().strftime('%m_%d_%H_%M_%S')
 	os.makedirs(os.path.join("experiments", run_name))
 	global_log = ""
-	prefix = "srun -N 1 -p gpu -t 720 "
+	prefix = "KMP_AFFINITY=granularity=fine,compact,1,0 srun -N 1 -p gpu -t 720 "
 	with open(os.path.join("experiments", run_name, "times.csv"), "w") as f:
-		f.write(algos[0] + ";")
-		for t in threads:
-			f.write(t + ';')
-		f.write("\n")
-		for m in matrices:
-			print(m)
-			f.write(os.path.basename(m) + ";")
-			snodes = m[:-6] + "snodes"
+		for a in algos:
+			f.write(a + ";")
 			for t in threads:
-				print(t)
-				string = prefix + os.path.join(os.getcwd(), filename + " " + algos[0] + " " + m + " " + snodes + " " + t)
-				print(string)
-				log, time = min_res(string, repeats=repeats)
-				global_log += log
-				print(log)
-				global_log += "-"*100+"\n"
-				f.write(str(time) + ";")
+				f.write(t + ';')
 			f.write("\n")
+			for m in matrices:
+				print(m)
+				f.write(os.path.basename(m) + ";")
+				snodes = m[:-6] + "snodes"
+				for t in threads:
+					print(t)
+					string = prefix + os.path.join(os.getcwd(), filename + " " + a + " " + m + " " + snodes + " " + t)
+					print(string)
+					log, time = min_res(string, repeats=repeats)
+					global_log += log
+					print(log)
+					global_log += "-"*100+"\n"
+					f.write(str(time) + ";")
+				f.write("\n")
 	with open(os.path.join("experiments", run_name, "best_times_log.log"), "w") as f:
 		f.write(global_log)
 
 def main():
 	filename = os.path.join("code", "SPTRSV")
-	algos = ["mkl"]
-	threads = ["1", "2", "4", "8", "16", "24", "32"]
+	algos = ["barrier", "mkl"]
+	threads = ["1", "2", "4", "8", "16", "24", "32", "64"]
 	matrices = glob(os.path.join(os.getcwd(), "matrices", "bin", "*"))
 	assert len(matrices) % 2 == 0
-	matrices = list(filter(lambda x: x.endswith('fem.matrix'), matrices))
-	repeats = 10
+	matrices = list(filter(lambda x: x.endswith('.matrix'), matrices))
+	repeats = 7
 
 	get_times(filename, algos, matrices, repeats, threads)
 
