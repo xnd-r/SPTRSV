6abbe62323f78c9b077653a57107a916007c6aeb
Alexander Romanov <romanov_a@master.nngu>
2021-04-30 23:02:29 +0300

    Serial algos experments with 100 right sides

--- run.py
+++ run.py
@@ -10,8 +10,15 @@ def single_launch(name):
 	proc.wait()
 	res = proc.communicate()
 	log = res[1].decode("utf-8")
-	time = float(log.split('\n')[-2].split("Time: ")[-1])
-	return log, time
+	try:
+		time = float(log.split('\n')[-2].split("Time: ")[-1])
+		return log, time
+	except ValueError as ve:
+		print(log)
+		with open(os.path.join(os.getcwd(), 'error_log.log'), 'r') as f:
+			f.write(log)
+		print("Runtime error. Exit")
+		exit(0)
 
 def min_res(name, repeats=3):
 	min_time = 1e6
@@ -23,44 +30,41 @@ def min_res(name, repeats=3):
 			min_log = log
 	return min_log, min_time
 
-def get_times(filename, algos, matrices, repeats, threads):
+def get_times(filename, algos, matrices, repeats):
 	run_name = datetime.datetime.now().strftime('%m_%d_%H_%M_%S')
 	os.makedirs(os.path.join("experiments", run_name))
 	global_log = ""
-	prefix = "KMP_AFFINITY=granularity=fine,compact,1,0 srun -N 1 -p gpu -t 720 "
+	prefix = "srun -N 1 -p gpu -t 720 "
 	with open(os.path.join("experiments", run_name, "times.csv"), "w") as f:
+		f.write(";")
 		for a in algos:
-			f.write(a + ";")
-			for t in threads:
-				f.write(t + ';')
+			f.write(a + ';')
+		f.write("\n")
+		for m in matrices:
+			print(m)
+			f.write(os.path.basename(m) + ";")
+			snodes = m[:-6] + "snodes"
+			for a in algos:
+				string = prefix + os.path.join(os.getcwd(), filename + " " + a + " " + m + " " + snodes + " " + "1 100")
+				print(string)
+				log, time = min_res(string, repeats=repeats)
+				global_log += log
+				global_log += "-"*100+"\n"
+				f.write(str(time) + ";")
 			f.write("\n")
-			for m in matrices:
-				print(m)
-				f.write(os.path.basename(m) + ";")
-				snodes = m[:-6] + "snodes"
-				for t in threads:
-					print(t)
-					string = prefix + os.path.join(os.getcwd(), filename + " " + a + " " + m + " " + snodes + " " + t + " 100")
-					print(string)
-					log, time = min_res(string, repeats=repeats)
-					global_log += log
-					print(log)
-					global_log += "-"*100+"\n"
-					f.write(str(time) + ";")
-				f.write("\n")
 	with open(os.path.join("experiments", run_name, "best_times_log.log"), "w") as f:
 		f.write(global_log)
 
 def main():
 	filename = os.path.join("code", "SPTRSV")
-	algos = ["barrier"]
-	threads = ["1", "2", "4", "8", "16", "24", "32", "64"]
-	matrices = glob(os.path.join(os.getcwd(), "matrices", "bin", "*"))
+	algos = ["base", "custom", "blas"]
+	matrices = glob("./matrices/bin/*")
+	print(matrices)
 	assert len(matrices) % 2 == 0
-	matrices = list(filter(lambda x: x.endswith('.matrix'), matrices))
-	repeats = 5
+	matrices = list(filter(lambda x: not x.endswith('.snodes'), matrices))
+	repeats = 3
 
-	get_times(filename, algos, matrices, repeats, threads)
+	get_times(filename, algos, matrices, repeats)
 
 
 if __name__ == "__main__":
