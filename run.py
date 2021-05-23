f77ebf7ff6574ddbdeb9b446fbe2817a92ebb912
Alexander Romanov <romanov_a@master.nngu>
2021-05-23 12:45:19 +0300

    Optimized Barrier algo, 100 rhs

--- run.py
+++ run.py
@@ -30,40 +30,45 @@ def min_res(name, repeats=3):
 			min_log = log
 	return min_log, min_time
 
-def get_times(filename, algos, matrices, repeats):
+def get_times(filename, algos, matrices, repeats, threads):
 	run_name = datetime.datetime.now().strftime('%m_%d_%H_%M_%S')
 	os.makedirs(os.path.join("experiments", run_name))
 	global_log = ""
-	prefix = "srun -N 1 -p gpu -t 720 "
+	prefix = "KMP_AFFINITY=granularity=fine,compact,1,0 srun -N 1 -p gpu -t 720 "
 	with open(os.path.join("experiments", run_name, "times.csv"), "w") as f:
-		f.write(";")
 		for a in algos:
-			f.write(a + ';')
-		f.write("\n")
-		for m in matrices:
-			print(m)
-			f.write(os.path.basename(m) + ";")
-			snodes = m[:-6] + "snodes"
-			for a in algos:
-				string = prefix + os.path.join(os.getcwd(), filename + " " + a + " " + m + " " + snodes + " " + "1 100 opt_true")
-				print(string)
-				log, time = min_res(string, repeats=repeats)
-				global_log += log
-				global_log += "-"*100+"\n"
-				f.write(str(time) + ";")
+			f.write(a + ";")
+			for t in threads:
+				f.write(t + ';')
 			f.write("\n")
+			for m in matrices:
+				print(m)
+				f.write(os.path.basename(m) + ";")
+				snodes = m[:-6] + "snodes"
+				for t in threads:
+					print(t)
+					string = prefix + os.path.join(os.getcwd(), filename + " " + a + " " + m + " " + snodes + " " + t + " 100 opt true")
+					print(string)
+					log, time = min_res(string, repeats=repeats)
+					global_log += log
+					print(log)
+					global_log += "-"*100+"\n"
+					f.write(str(time) + ";")
+				f.write("\n")
 	with open(os.path.join("experiments", run_name, "best_times_log.log"), "w") as f:
 		f.write(global_log)
 
 def main():
 	filename = os.path.join("code", "SPTRSV")
-	algos = ["base", "custom"]
+	algos = ["barrier"]
+	threads = ["1", "2", "4", "8", "16", "24", "32"]
 	matrices = glob("./matrices/bin/*")
+	print(matrices)
 	assert len(matrices) % 2 == 0
 	matrices = list(filter(lambda x: not x.endswith('.snodes'), matrices))
-	repeats = 4
+	repeats = 3
 
-	get_times(filename, algos, matrices, repeats)
+	get_times(filename, algos, matrices, repeats, threads)
 
 
 if __name__ == "__main__":
